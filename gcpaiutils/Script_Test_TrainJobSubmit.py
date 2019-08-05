007f449b840b78d61bacd84fffe92f854a065fcd
Enrico Testa <enrico@mailtesta.com>
2019-08-05 07:06:39 +0200

    Fixed path

--- gcpaiutils/Script_Test_TrainJobSubmit.py
+++ gcpaiutils/Script_Test_TrainJobSubmit.py
@@ -4,7 +4,7 @@ from os import getcwd
 PROJECT_NAME = "num-00"
 
 S = JobSpecHandler(PROJECT_NAME)
-S.create_job_specs(getcwd() + '/gcpaiutils/train/default_job_specs_docker.yml')
+S.create_job_specs(getcwd() + '/train/default_job_specs_docker.yml')
 
 T = TrainJobHandler(PROJECT_NAME)
 T.submit_train_job(S.job_specs)
