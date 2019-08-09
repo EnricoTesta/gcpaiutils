5dc0a34e81bb890f9d30fa40627f3172536c0a89
Enrico Testa <enrico@mailtesta.com>
2019-08-09 07:58:31 +0200

    updated with new test account

--- gcpaiutils/Script_Test_TrainJobSubmit.py
+++ gcpaiutils/Script_Test_TrainJobSubmit.py
@@ -1,6 +1,6 @@
 from gcpaiutils.train.trainjobs import TrainJobHandler, JobSpecHandler
 
-S = JobSpecHandler(algorithm='class_skl_logreg', train_inputs={'modelDir': 'gs://ml_train_deploy_test/test_87/'})
+S = JobSpecHandler(algorithm='class_skl_logreg', train_inputs={'modelDir': 'gs://my_ml_test_bucket/container_test/'})
 S.create_job_specs()
 
 T = TrainJobHandler(job_executor='gcloud')
