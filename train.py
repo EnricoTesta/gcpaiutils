0c3e24454f8ba080511d6f4894d4f1eafb98ae62
Enrico Testa <enrico@mailtesta.com>
2019-09-24 16:49:06 +0200

    subclass from handler.py

--- train.py
+++ train.py
@@ -1,37 +1,14 @@
-"""
-
-Implements custom class to submit train jobs.
-
-IMPORTANT: appears that currently there is no way of training using an existing tarball in cloud.
-Procedure gives the error: FileNotFoundError: [Errno 2] No such file or directory: '/tmp/pip-req-build-_6j89qcg/setup.py'
-
-The only way I successfully packaged a training application at the moment is via gcloud (specifying a local path).
-This means that for each training application I must first submit via gcloud to package it, then I can use it indefinitely
-for training using the discovery api.
-
-"""
-from googleapiclient import discovery, errors
+from googleapiclient import discovery
 from datetime import datetime as dt
-from yaml import safe_load
-import google.auth
-import logging
+from handler import JobHandler, JobSpecHandler, GLOBALS, DEFAULTS, HYPER
 import subprocess
-import os
 
 
 JOB_SPECS_GLOBAL_ARGS = ['scaleTier', 'region', 'modelDir']
 JOB_SPECS_DEFAULT_ARGS = ['args', 'trainFiles']
 
-# Load globals and defaults
-with open('/gcpaiutils/config/deployment.yml', 'r') as stream:
-    GLOBALS = safe_load(stream)
-with open('/gcpaiutils/config/defaults.yml', 'r') as stream:
-    DEFAULTS = safe_load(stream)
-with open('/gcpaiutils/config/hypertune.yml', 'r') as stream:
-    HYPER = safe_load(stream)
 
-
-class TrainJobHandler:
+class TrainJobHandler(JobHandler):
     """Builds train request for GCP AI Platform. Requires job specification as produced by JobSpecHandler.
 
        Args:
@@ -40,30 +17,11 @@ class TrainJobHandler:
                            the latter uses google's discovery api.
 
         Main usage:
-           - submit_train_job(): returns the object. Sends the job request (async) with the specified parameters.
+           - submit_job(): returns the object. Sends the job request (async) with the specified parameters.
     """
     def __init__(self, project_name=GLOBALS['PROJECT_NAME'], job_executor='gcloud'):
-        self._project_name = project_name
-        self.job_executor = job_executor
-        self.mlapi = None
+        super().__init__(project_name, job_executor)
         self.hypertune = False
-        self._credentials = None
-        self._project_id = None
-        self.job_request = None
-        self.success = None
-
-    def _auth_setup(self):
-        # TODO: do not use environment variables but instead build discovery api with auth file
-        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = GLOBALS['GOOGLE_APPLICATION_CREDENTIALS_JSON']
-        self._credentials, self._project_id = google.auth.default()
-
-    def _execute_job_request(self, job_spec):
-        if self.job_executor == 'gcloud':
-            self._exe_job_gcloud(job_spec)
-        elif self.job_executor == 'mlapi':
-            self._exe_job_mlapi()
-        else:
-            raise NotImplementedError
 
     def _exe_job_gcloud(self, job_spec):
         prefix = 'export PATH=/home/vagrant/google-cloud-sdk/bin:$PATH && '
@@ -95,14 +53,6 @@ class TrainJobHandler:
         subprocess.run(submit_cmd, shell=True, check=True)
         self.success = True
 
-    def _exe_job_mlapi(self):
-        try:
-            self.job_request.execute()  # TODO: manage output (jobId, state, ...)
-            self.success = True
-        except errors.HttpError as err:
-            logging.error(err._get_reason())
-            self.success = False
-
     def create_job_request(self, job_spec=None):
         if job_spec is None:
             raise ValueError("Must set job_spec to create a train job.")
@@ -121,13 +71,8 @@ class TrainJobHandler:
         self.job_request = self.mlapi.projects().jobs().create(body=job_spec
                                                                , parent='projects/{}'.format(self._project_name))
 
-    def submit_train_job(self, job_spec):
-        if self.job_executor == 'mlapi':
-            self.create_job_request(job_spec)
-        self._execute_job_request(job_spec)
 
-
-class JobSpecHandler:
+class TrainJobSpecHandler(JobSpecHandler):
     """Builds job specifications to submit to GCP AI Platform. Specifications can be specified via a dictionary.
     If dictionary is not provided the class fetches defaults from a configuration file.
 
@@ -141,18 +86,10 @@ class JobSpecHandler:
 
     """
 
-    def __init__(self, algorithm=None, project_name=GLOBALS['PROJECT_NAME'], train_inputs={}, hypertune=False):
-        self.algorithm = algorithm
-        self._train_inputs = train_inputs
-        try:
-            self._train_inputs['imageUri'] = GLOBALS['ATOMS'][self.algorithm]
-        except KeyError:
-            raise ValueError("Unknown algorithm")
-        self._project_name = project_name
-        self._train_inputs = train_inputs
+    def __init__(self, algorithm=None, project_name=None, inputs={}, hypertune=False):
+        super().__init__(algorithm, project_name, inputs)
+        self._train_inputs = inputs
         self.hypertune = hypertune
-
-        self.job_specs = None
 
     def _generate_job_name(self):
         """Generates jobId (mixed-case letters, numbers, and underscores only, starting with a letter).
@@ -169,14 +106,12 @@ class JobSpecHandler:
         # job name must start with a letter and string must be lowercase
         if self._train_inputs['trainFiles']:
             shards = self._train_inputs['trainFiles'].split("/")  # 3 = subject / 4 = problem / 6 = version
-            return shards[3].lower() + '_' + shards[4].lower() + '_' + shards[6].lower() + '_' + \
+            return 'train_' + shards[3].lower() + '_' + shards[4].lower() + '_' + shards[6].lower() + '_' + \
                    year + month + day + hour + minute + second + '_' + \
-                   self._train_inputs['imageUri'][0].split("/")[-1].split(":")[1].lower() + '_' + \
-                   self._train_inputs['scaleTier'].lower()
+                   self.algorithm + '_' + self._train_inputs['scaleTier'].lower()
         else:
-            return 'j_' + year + month + day + hour + minute + second + '_' + \
-                self._train_inputs['imageUri'][0].split("/")[-1].split(":")[1].lower() + '_' + \
-                self._train_inputs['scaleTier'].lower()
+            return 'trainjob_' + year + month + day + hour + minute + second + '_' + \
+                self.algorithm + '_' + self._train_inputs['scaleTier'].lower()
 
     def create_job_specs(self):
 
@@ -203,21 +138,3 @@ class JobSpecHandler:
 
         self._train_inputs['modelDir'] = self._train_inputs['modelDir'] + job_id + '/'
         self.job_specs = {'jobId': job_id, 'trainingInput': self._train_inputs}
-
-
-# TODO: switch to discovery API to submit train requests. This way you can add an HyperparameterSpec object to the dict
-"""
-# Job Request with HPT
-export BUCKET_NAME=ml_train_deploy_test
-export MODEL_DIR=sklearn_model_$(date +%Y%m%d_%H%M%S)
-export REGION=us-central1
-export JOB_NAME=custom_container_job_$(date +%Y%m%d_%H%M%S)
-
-gcloud beta ai-platform jobs submit training $JOB_NAME \
-  --region $REGION \
-  --master-image-uri $IMAGE_URI \
-  --config /Numerai_2.0/mlatoms/ht_config.yml \
-  -- \
-  --model-dir=gs://$BUCKET_NAME/$MODEL_DIR \
-  --train-files=gs://$BUCKET_NAME/sample_data/sample_data.csv
-"""
