bf6ed82f23297c04c832eb1e35a58a5bbc59832c
Enrico Testa <enrico@mailtesta.com>
2019-10-02 07:20:01 +0200

    add get_model_path_from_info_path

--- utils.py
+++ utils.py
@@ -2,3 +2,9 @@ def get_atom_name_from_dir(job_dir):
     job_name = job_dir.split("/")[-2]
     name_shards = job_name.split("_")
     return "_".join(name_shards[4:-1])
+
+
+def get_model_path_from_info_path(info_path):
+    shards = info_path.split(".")[0].split("_")
+    idx = shards.index("info")
+    return '_'.join(shards[0:idx]) + '/model_' + '_'.join(shards[idx+1:]) + ".pkl"
