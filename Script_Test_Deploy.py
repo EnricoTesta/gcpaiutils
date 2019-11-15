27406699ae068acdb9e2f1b0fd30e0f0daae3cb8
Enrico Testa <enrico@mailtesta.com>
2019-11-15 06:45:53 +0100

    clean up

--- Script_Test_Deploy.py
+++
@@ -1,6 +0,0 @@
-from deploy import DeploymentHandler
-
-ORIGIN = "gs://my_core_model_bucket/j20190813041413_class_xgb_basic/"
-
-d = DeploymentHandler()
-d.deploy(model_name="XGB_Final", model_version="V3", model_origin=ORIGIN, build_model=False)
