430d3a3a39b044a46ffdcdb6b87c1521a8abb8f3
Enrico Testa <enrico@mailtesta.com>
2020-01-13 06:48:34 +0100

    update dependencies with google-cloud-storage

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     ],
     python_requires='>=3.5',
     install_requires=["google-api-python-client>=1.7.10",
-                      "google-cloud>=0.34.0",
+                      "google-cloud-storage>=1.19.1",
                       "google-auth>=1.6.3",
                       "PyYAML>=5.1.2",
                       "Jinja2>=2.10.3"]
