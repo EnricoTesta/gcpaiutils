7480a5e4989ad9c069b87b8e9e91526e527fc4c3
Enrico Testa <enrico@mailtesta.com>
2020-02-01 10:24:32 +0100

    removed gcloud execution. Removed _generate_job_name implementation. Refactored _train_inputs in inputs. Edited to match new handlers interface.

--- gcpaiutils/train.py
+++ gcpaiutils/train.py
@@ -23,37 +23,7 @@ class TrainJobHandler(JobHandler):
         super().__init__(deployment_config, job_executor)
         self.hypertune = False
 
-    def _exe_job_gcloud(self, job_spec):
-        prefix = 'export PATH=/home/vagrant/google-cloud-sdk/bin:$PATH && '
-        gcloud = 'gcloud beta ai-platform jobs submit training '
-        name = job_spec['jobId'] + ' '
-        region = '--region ' + job_spec['trainingInput']['region'] + ' '
-        image = '--master-image-uri ' + job_spec['trainingInput']['imageUri'] + ' '
-        scale = '--scale-tier ' + job_spec['trainingInput']['scaleTier'].lower() + ' '
-        if job_spec['trainingInput']['scaleTier'].lower() == 'custom':
-            master_machine_type = '--master-machine-type ' + job_spec['trainingInput']['masterType'] + ' '
-        else:
-            master_machine_type = ''
-        if self.hypertune:
-            hyper = self.hypertune
-        else:
-            hyper = ''
-        pause = '-- '
-        modeldir = '--model-dir=' + job_spec['trainingInput']['modelDir'] + ' '
-        train_files = '--train-files=' + job_spec['trainingInput']['trainFiles'] + ' '
-
-        # User-defined args. Even position specify argument name. Odd positions argument values.
-        user_defined_args = []
-        for i, element in enumerate(job_spec['trainingInput']['args']):
-            if i % 2 == 0:  # argument name
-                user_defined_args.append('--' + element + '=' + str(job_spec['trainingInput']['args'][i+1]))
-
-        submit_cmd = prefix + gcloud + name + region + image + scale + master_machine_type + hyper + pause + \
-                     modeldir + train_files + ' '.join(user_defined_args)
-        subprocess.run(submit_cmd, shell=True, check=True)
-        self.success = True
-
-    def create_job_request(self, job_spec=None):
+    def translate_job_specs(self, job_spec=None):
         if job_spec is None:
             raise ValueError("Must set job_spec to create a train job.")
         if 'hyperparameters' in job_spec['trainingInput']:
@@ -72,10 +42,7 @@ class TrainJobHandler(JobHandler):
         if self.hypertune:
             job_spec['trainingInput']['args'] += ['--hypertune-loss', job_spec['trainingInput'].pop('hypertuneLoss')]
 
-        self.success = None  # reset success flag
-        self.mlapi = discovery.build('ml', 'v1', credentials=self._credentials)
-        self.job_request = self.mlapi.projects().jobs().create(body=job_spec
-                                                               , parent='projects/{}'.format(self._project_id))
+        return job_spec
 
 
 class TrainJobSpecHandler(JobSpecHandler):
@@ -91,49 +58,15 @@ class TrainJobSpecHandler(JobSpecHandler):
           Platform request.
 
     """
-    def __init__(self, deployment_config=None, algorithm=None, inputs={}, append_job_id=True, hypertune=False):
-        super().__init__(deployment_config=deployment_config, algorithm=algorithm,
+    def __init__(self, deployment_config=None, algorithm=None, inputs={}, append_job_id=True,
+                 request_ids=None, hypertune=False):
+        super().__init__(deployment_config=deployment_config, algorithm=algorithm, request_ids=request_ids,
                          append_job_id=append_job_id, inputs=inputs)
         try:
             self.inputs["imageUri"] = self._deployment['ATOMS'][self.algorithm][0]
         except KeyError:
-            # TODO: currently Aggregator is considered a training atom but it's not found within training atoms list
-            try:
-                self.inputs["imageUri"] = self._deployment['POSTPROCESS'][self.algorithm][0]
-            except KeyError:
-                raise ValueError("Unknown algorithm")
-        self._train_inputs = inputs
+            raise ValueError("Unknown algorithm")
         self.hypertune = hypertune
-
-    def _generate_job_name(self):
-        """Generates jobId (mixed-case letters, numbers, and underscores only, starting with a letter).
-        Include info about hardware (scaleTier), training algo (packageUris), train data (--train-files)."""
-
-        n = dt.now()
-        year = str(n.year)
-        month = str(n.month) if n.month > 9 else '0' + str(n.month)
-        day = str(n.day) if n.day > 9 else '0' + str(n.day)
-        hour = str(n.hour) if n.hour > 9 else '0' + str(n.hour)
-        minute = str(n.minute) if n.minute > 9 else '0' + str(n.minute)
-        second = str(n.second) if n.second > 9 else '0' + str(n.second)
-
-        # job name must start with a letter and string must be lowercase
-        try:
-        #if self._train_inputs['trainFiles']:
-            shards = self._train_inputs['trainFiles'].split("/")  # 3 = subject / 4 = problem / 6 = version
-            return 'train_' + shards[3].lower() + '_' + shards[4].lower() + '_' + shards[6].lower() + '_' + \
-                   year + month + day + hour + minute + second + '_' + \
-                   self.algorithm + '_' + self._train_inputs['scaleTier'].lower()
-        except:
-            #else:
-            try:
-                shards = self._train_inputs['modelDir'].split("/")  # 3 = subject / 4 = problem / 5 = version
-                return 'train_' + shards[3].lower() + '_' + shards[4].lower() + '_' + shards[5].lower() + '_' + \
-                       year + month + day + hour + minute + second + '_' + \
-                       self.algorithm + '_' + self._train_inputs['scaleTier'].lower()
-            except:
-                return 'trainjob_' + year + month + day + hour + minute + second + '_' + \
-                    self.algorithm + '_' + self._train_inputs['scaleTier'].lower()
 
     def create_job_specs(self):
 
@@ -143,25 +76,25 @@ class TrainJobSpecHandler(JobSpecHandler):
 
         # Cast defaults if not found
         for item in spec_full_args:
-            if item in self._train_inputs:
+            if item in self.inputs:
                 continue
 
             if item in JOB_SPECS_GLOBAL_ARGS:
                 if item == "modelDir":
-                    self._train_inputs[item] = self._globals["MODEL_BUCKET_ADDRESS"]
+                    self.inputs[item] = self._globals["MODEL_BUCKET_ADDRESS"]
                 else:
-                    self._train_inputs[item] = self._globals[item]
+                    self.inputs[item] = self._globals[item]
             elif item in JOB_SPECS_DEFAULT_ARGS:
-                self._train_inputs[item] = self._defaults[self.algorithm][item]
+                self.inputs[item] = self._defaults[self.algorithm][item]
             elif item == 'hyperparameters':
-                self._train_inputs[item] = self._hyper[self.algorithm]
+                self.inputs[item] = self._hyper[self.algorithm]
             elif item == 'hypertuneLoss':
-                self._train_inputs[item] = self._train_inputs["hyperparameters"]["hyperparameterMetricTag"]
+                self.inputs[item] = self.inputs["hyperparameters"]["hyperparameterMetricTag"]
             else:
                 raise NotImplementedError("Unrecognized job spec argument %s" % item)
 
         # Generate jobId
-        job_id = self._generate_job_name()
-
-        self._train_inputs['modelDir'] = self._train_inputs['modelDir'] + '_'.join(job_id.split("_")[1:]) + '/'
-        self.job_specs = {'jobId': job_id, 'trainingInput': self._train_inputs}
+        job_id = self._generate_job_name(prefix='train')
+        if self.append_job_id:
+            self.inputs['modelDir'] = self.inputs['modelDir'] + '_'.join(job_id.split("_")[1:]) + '/'
+        self.job_specs = {'jobId': job_id, 'trainingInput': self.inputs}
