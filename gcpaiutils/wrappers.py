b69caa4668ad9049dc8bcc4bac4d1512bb2fd71d
Enrico Testa <enrico@mailtesta.com>
2020-02-08 15:26:27 +0100

    add wait_dag_status, clear_dag_status, notify_dag_status. Refactor data_evaluation, train, score, selection, aggregate. Set masterType to n1-highmem-8 for data_evaluation.

--- gcpaiutils/wrappers.py
+++ gcpaiutils/wrappers.py
@@ -3,10 +3,10 @@ from gcpaiutils.predict import ScoreJobHandler, ScoreJobSpecHandler
 from gcpaiutils.postprocess import PostprocessJobHandler, PostprocessJobSpecHandler
 from gcpaiutils.preprocess import PreprocessJobHandler, PreprocessJobSpecHandler
 from gcpaiutils.utils import get_model_path_from_info_path, get_deployment_config, get_hardware_config,\
-    get_user, get_problem, get_version, get_gcs_credentials, make_temp_dir, get_job_assessment, get_selector
+    get_user, get_problem, get_version, get_gcs_credentials, make_temp_dir, get_job_assessment,\
+    get_selector, get_metadata
 from googleapiclient import discovery
 from google.cloud import storage
-from subprocess import check_call
 from shutil import rmtree
 import logging
 from google.oauth2.service_account import Credentials
@@ -66,187 +66,7 @@ def poll(deployment_config, time_interval, jobs):
     return status
 
 
-def train(deployment_config, atom=None, train_files=None, master_type=None, hyperspace=None, **kwargs):
-    """
-    Submits train job to GCP AI Platform and waits for completion.
-
-    :param deployment_config: YAML file containing all deployment variables
-    :param atom: algorithm name as tagged in Container Registry (atom)
-    :param train_files: .csv file(s) complete URI
-    :param master_type: GCP VM type to use during training
-    :param hyperspace: hyper-parameter tuning configuration
-    :param kwargs:
-    :return:
-    """
-    hypertune = hyperspace is not None
-    trainingInput = {
-        "trainFiles": train_files,
-        "scaleTier": "CUSTOM"
-    }
-
-    submitted_jobs = []
-
-    current_train_input = trainingInput.copy()
-    current_train_input["masterType"] = master_type
-
-    if hypertune:
-        current_train_input["hyperparameters"] = hyperspace[atom]
-        current_train_input["hypertuneLoss"] = hyperspace[atom]["hyperparameterMetricTag"]
-
-    S = TrainJobSpecHandler(deployment_config=deployment_config, algorithm=atom, inputs=current_train_input,
-                            hypertune=hypertune)
-    S.create_job_specs()
-    T = TrainJobHandler(deployment_config=deployment_config, job_executor='mlapi')
-    T.submit_job(S.job_specs)
-    if T.success:
-        submitted_jobs.append(S.job_specs['jobId'])
-        logging.info("Train request successful: {}".format(S.job_specs['jobId']))
-    else:
-        raise ValueError("Unable to submit train job.")
-
-    status = poll(deployment_config, TIME_INTERVAL, submitted_jobs)
-
-    successful_jobs = [job for job, s in status.items() if s == 'SUCCEEDED']
-    failed_jobs = [job for job, s in status.items() if s == 'FAILED']
-    if len(failed_jobs) >= 1:
-        for job in failed_jobs:
-            logging.error("Train job failed: {}".format(job))
-        raise ValueError("One or more train jobs failed")
-    task_instance = kwargs['task_instance']
-    task_instance.xcom_push(key='successful_jobs', value=successful_jobs)
-
-
-def selection(deployment_config, train_task_ids=None, selector_class=None, **kwargs):
-    """
-    Selects among trained models those that will make it to production.
-
-    :param deployment_config: YAML file containing all deployment variables
-    :param train_task_ids: list of training tasks that represent the universe on which selection takes place
-    :param selector_class: customized BaseSelector object
-    :param kwargs:
-    :return:
-    """
-    GLOBALS = get_deployment_config(deployment_config)
-
-    successful_train_jobs = []
-    for train_task in train_task_ids:
-        successful_train_jobs.append(kwargs['task_instance'].xcom_pull(task_ids=train_task, key='successful_jobs'))
-    successful_train_jobs = [item for sublist in successful_train_jobs for item in sublist]  # flatten
-
-    # Call a selection method
-    info_dir = "{}/info_tmp".format(os.getcwd())
-    if os.path.exists(info_dir):
-        rmtree(info_dir)
-    os.mkdir(info_dir)
-
-    for job in successful_train_jobs:
-        # download in dir with job name
-        tmp_dir_name = "{}/{}".format(info_dir, job.replace("train_", ""))
-        os.mkdir(tmp_dir_name)
-        # TODO: need to configure gsutil with new deployment
-        cmd = 'gsutil -m cp {}{}/info_*.csv {}/{}'.format(GLOBALS["MODEL_BUCKET_ADDRESS"],
-                                                           job.replace("train_", ""), info_dir,
-                                                           job.replace("train_", ""))
-        os.system(cmd)
-
-        # concatenate file name to match GCS flat namespace
-        for file in [f for f in os.listdir(tmp_dir_name) if os.path.isfile(os.path.join(tmp_dir_name, f))]:
-            os.rename(os.path.join(tmp_dir_name, file),
-                      os.path.join(info_dir, '_'.join([job.replace("train_", ""), file])))
-
-        # remove dir
-        rmtree(tmp_dir_name)
-
-    # make sure selector_class is imported in this module
-    S = selector_class(model_dir=info_dir, problem_type='classification', n_class=5, verbose=True)
-    selected_info = S.select()
-    rmtree(info_dir)
-    kwargs['task_instance'].xcom_push(key='selected_info', value=selected_info)
-
-
-def score(deployment_config, selection_task_id=None, score_dir=None, use_proba=None, master_type=None,
-          subject=None, problem=None, version=None, **kwargs):
-    """
-    Submits score job(s) to GCP AI Platform and waits for completion.
-
-    :param deployment_config: YAML file containing all deployment variables
-    :param selection_task_id: tasks that contains models to score
-    :param score_dir: URI where to write results
-    :param use_proba: (string). 0 = No / 1 = Yes
-    :param master_type: GCP VM type to use during scoring
-    :param subject: (string)
-    :param problem: (string)
-    :param version: (string)
-    :param kwargs:
-    :return:
-    """
-    task_instance = kwargs['task_instance']
-    selected_info = task_instance.xcom_pull(task_ids=selection_task_id, key='selected_info')
-
-    scoreInput = {
-        "scoreDir": score_dir,
-        "useProba": use_proba,
-        "masterType": master_type,
-        "scaleTier": "CUSTOM"
-    }
-
-    GLOBALS = get_deployment_config(deployment_config)
-    submitted_scoring_jobs = {}
-
-    for info in selected_info:
-
-        currentInput = scoreInput.copy()
-        model_path = get_model_path_from_info_path(info)
-
-        # Look for model in appropriate blob (temporary - assumes single model in blob)
-        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/gauth/z-account-main-project-0-ai-platform-default.json'  # TODO: use OAuth2 credentials
-        # storage_client = storage.Client(credentials=storage_credentials)
-        storage_client = storage.Client()
-        blobs = list(storage_client.list_blobs(GLOBALS["MODEL_BUCKET_NAME"], prefix=model_path))  # unique id
-        currentInput["modelFile"] = os.path.join(GLOBALS["MODEL_BUCKET_ADDRESS"], blobs[0].name)
-        currentInput["outputDir"] = "gs://{}/{}/{}/RESULTS/{}/{}/".format(GLOBALS["CORE_BUCKET_NAME"],
-                                                                          subject,
-                                                                          problem, version, model_path.split("/")[0])
-
-        S = ScoreJobSpecHandler(algorithm='_'.join(model_path.split("/")[0].split("_")[4:-1]),
-                                deployment_config=deployment_config,
-                                inputs=currentInput)
-        S.create_job_specs()
-        T = ScoreJobHandler(deployment_config=deployment_config, job_executor='mlapi')
-        T.submit_job(S.job_specs)
-        if T.success:
-            submitted_scoring_jobs[S.job_specs['jobId']] = model_path.split("/")[0]  # corresponding train job id
-            logging.info("Score request successful: {}".format(S.job_specs['jobId']))
-        else:
-            raise ValueError("Unable to submit score job.")
-
-    # Retrieve scoring
-    status = poll(deployment_config, TIME_INTERVAL, submitted_scoring_jobs)
-    successful_scoring_jobs = {}
-    for job, s in status.items():
-        if s == 'SUCCEEDED':
-            successful_scoring_jobs[job] = submitted_scoring_jobs[job]  # keep reference to corresponding training job
-        else:
-            raise ValueError("One or more score jobs failed")
-
-    pred_dir = "{}/pred_dir".format(os.getcwd())
-    if os.path.exists(pred_dir):
-        rmtree(pred_dir)
-    os.mkdir(pred_dir)
-
-    for _, train_job in successful_scoring_jobs.items():  # hp that all score jobs are successful and sorted
-        cmd = "gsutil cp gs://{}/{}/{}/RESULTS/{}/{}/results.csv {}/{}.csv".format(GLOBALS["CORE_BUCKET_NAME"],
-                                                                                   subject, problem,
-                                                                                   version,
-                                                                                   train_job,
-                                                                                   pred_dir,
-                                                                                   train_job)
-        check_call(cmd, shell=True)
-
-    kwargs['task_instance'].xcom_push(key='pred_dir', value=pred_dir)
-
-
-def train_new(deployment_config, atom=None, hyperspace=None, **kwargs):
+def train(deployment_config, atom=None, hyperspace=None, **kwargs):
     """
     Submits train job to GCP AI Platform and waits for completion. Compute is done remotely.
 
@@ -269,12 +89,11 @@ def train_new(deployment_config, atom=None, hyperspace=None, **kwargs):
         "scaleTier": "CUSTOM"
     }
 
-    # TODO: retrieve from file
-    data_size = kwargs['task_instance'].xcom_pull(task_ids='data_evaluation', key='data_size')
+    # Get metadata file
+    metadata = get_metadata(_globals, 'TRAIN', kwargs)
 
     submitted_jobs = []
-
-    trainingInput["masterType"] = get_hardware_config(atom, data_size)
+    trainingInput["masterType"] = get_hardware_config(atom, metadata['size'])
 
     if hypertune:
         trainingInput["hyperparameters"] = hyperspace[atom]
@@ -297,7 +116,7 @@ def train_new(deployment_config, atom=None, hyperspace=None, **kwargs):
     kwargs['task_instance'].xcom_push(key='successful_jobs', value=get_job_assessment(status))
 
 
-def selection_new(deployment_config, train_task_ids=None, selector_class=None, **kwargs):
+def selection(deployment_config, train_task_ids=None, selector_class=None, **kwargs):
     """
     Selects among trained models those that will make it to production. Compute is done locally.
 
@@ -353,7 +172,7 @@ def selection_new(deployment_config, train_task_ids=None, selector_class=None, *
     kwargs['task_instance'].xcom_push(key='selected_info', value=selected_info)
 
 
-def score_new(deployment_config, use_proba=None, master_type=None, **kwargs):
+def score(deployment_config, use_proba=None, **kwargs):
     """
     Submits score job(s) to GCP AI Platform and waits for completion. Compute is done remotely.
 
@@ -366,14 +185,14 @@ def score_new(deployment_config, use_proba=None, master_type=None, **kwargs):
     """
 
     _globals = get_deployment_config(deployment_config)
-    # TODO: retrieve data evaluation from file to assess hardware configuration and remove explicit master_type
 
+    # Get metadata file
+    metadata = get_metadata(_globals, 'SCORE', kwargs)
     score_dir = kwargs['task_instance'].xcom_pull(task_ids='retrieve_params', key='data_uri')
 
     scoreInput = {
         "scoreDir": score_dir,
         "useProba": use_proba,
-        "masterType": master_type,
         "scaleTier": "CUSTOM"
     }
 
@@ -392,19 +211,18 @@ def score_new(deployment_config, use_proba=None, master_type=None, **kwargs):
 
         currentInput = scoreInput.copy()
         model_path = get_model_path_from_info_path(info)
-
-        # storage_client = storage.Client(credentials=storage_credentials)
+        algo = '_'.join(model_path.split("/")[0].split("_")[4:])
         blobs = list(gcs_client.list_blobs(_globals["MODEL_BUCKET_NAME"],
                                            prefix=os.path.join(get_user(kwargs), get_problem(kwargs),
                                                                get_version(kwargs), "MODELS", model_path)))  # unique id
         currentInput["modelFile"] = os.path.join(_globals["MODEL_BUCKET_ADDRESS"], blobs[0].name)
-
         currentInput["outputDir"] = "gs://{}/{}/{}/{}/RESULTS_STAGING/{}/".format(_globals["MODEL_BUCKET_NAME"],
                                                                                   get_user(kwargs), get_problem(kwargs),
                                                                                   get_version(kwargs),
                                                                                   model_path.split("/")[0])
+        currentInput["masterType"] = get_hardware_config(algo, metadata['size'])
 
-        S = ScoreJobSpecHandler(algorithm='_'.join(model_path.split("/")[0].split("_")[4:-1]),
+        S = ScoreJobSpecHandler(algorithm=algo,
                                 deployment_config=deployment_config,
                                 inputs=currentInput, request_ids={'user': get_user(kwargs),
                                                                   'problem': get_problem(kwargs),
@@ -423,7 +241,7 @@ def score_new(deployment_config, use_proba=None, master_type=None, **kwargs):
     kwargs['task_instance'].xcom_push(key='successful_jobs', value=get_job_assessment(status))
 
 
-def aggregate_new(deployment_config, **kwargs):
+def aggregate(deployment_config, **kwargs):
     """
     Aggregate model scoring. Compute is done remotely.
 
@@ -439,13 +257,16 @@ def aggregate_new(deployment_config, **kwargs):
         selector_dict = json.load(f)
     rmtree(local_dir)
 
+    # Get metadata file
+    metadata = get_metadata(_globals, 'SCORE', kwargs)
+
     scoreInput = {
         "scoreDir": "gs://{}/{}/{}/{}/RESULTS_STAGING/".format(_globals["MODEL_BUCKET_NAME"],
                                                                get_user(kwargs),
                                                                get_problem(kwargs),
                                                                get_version(kwargs)),
         "outputDir": kwargs['task_instance'].xcom_pull(task_ids='retrieve_params', key='output_uri'),
-        "masterType": 'n1-standard-4',  # TODO: retrieve dynamically
+        "masterType": get_hardware_config('aggregator', metadata['size']),
         "scaleTier": "CUSTOM"
     }
 
@@ -473,19 +294,18 @@ def aggregate_new(deployment_config, **kwargs):
         raise NotImplementedError("Only supported aggregation is: 'average'")
 
 
-def data_evaluation(deployment_config, **kwargs):
+def data_evaluation(deployment_config, dag_type, **kwargs):
 
     _globals = get_deployment_config(deployment_config)
     data_path = kwargs['task_instance'].xcom_pull(task_ids='retrieve_params', key='data_uri')
-    # data_path = kwargs['data_path']
-    # model_dir = "gs://{}/ET/NUMER/TEST/METADATA/".format(_globals["MODEL_BUCKET_NAME"])
-    model_dir = "gs://{}/{}/{}/{}/METADATA/".format(_globals["MODEL_BUCKET_NAME"],
-                                                    get_user(kwargs), get_problem(kwargs), get_version(kwargs))
+    model_dir = "gs://{}/{}/{}/{}/METADATA/{}/".format(_globals["MODEL_BUCKET_NAME"],
+                                                       get_user(kwargs), get_problem(kwargs),
+                                                       get_version(kwargs), dag_type)
 
     preprocess_input = {'trainFiles': data_path,
                         'modelDir': model_dir,
                         'scaleTier': 'CUSTOM',
-                        'masterType': 'n1-standard-4'
+                        'masterType': 'n1-highmem-8'  # 8 vCPUs / 52 GB RAM
                         }
 
     S = PreprocessJobSpecHandler(deployment_config=deployment_config,
@@ -507,3 +327,60 @@ def data_evaluation(deployment_config, **kwargs):
     # Retrieve scoring
     status = poll(deployment_config, TIME_INTERVAL, submitted_preprocess_jobs)
     kwargs['task_instance'].xcom_push(key='successful_jobs', value=get_job_assessment(status))
+
+
+def wait_dag_status(deployment_config, dag_type, conf, **kwargs):
+
+    _globals = get_deployment_config(deployment_config)
+
+    gcs_credentials = get_gcs_credentials(_globals)
+    gcs_client = storage.Client(project=_globals['PROJECT_ID'], credentials=gcs_credentials)
+
+    while True:
+        sleep(60)
+        gcs_blob_list = list(gcs_client.list_blobs(bucket_or_name=_globals["MODEL_BUCKET_NAME"],
+                                                   prefix=os.path.join(conf['user'], conf['problem'],
+                                                                       conf['version'], "STATUS", dag_type)))
+        if len(gcs_blob_list) > 1:
+            raise ValueError("Found more than one status file")
+        elif len(gcs_blob_list) == 1:
+            if gcs_blob_list[0].name.split("/")[-1] == 'success.json':
+                break
+            elif gcs_blob_list[0].name.split("/")[-1] == 'failure.json':
+                raise ValueError("DAG failed")
+            else:
+                raise ValueError("Unknown status file: %s" % gcs_blob_list[0].name.split("/")[-1])
+
+
+def clear_dag_status(deployment_config, dag_type, conf, **kwargs):
+
+    _globals = get_deployment_config(deployment_config)
+    gcs_credentials = get_gcs_credentials(_globals)
+    gcs_client = storage.Client(project=_globals['PROJECT_ID'], credentials=gcs_credentials)
+
+    gcs_blob_list = list(gcs_client.list_blobs(bucket_or_name=_globals["MODEL_BUCKET_NAME"],
+                                               prefix=os.path.join(conf['user'], conf['problem'],
+                                                                   conf['version'], "STATUS", dag_type)))
+    for blob in gcs_blob_list:
+        blob.delete()
+
+
+def notify_dag_status(deployment_config, dag_type, status, **kwargs):
+
+    _globals = get_deployment_config(deployment_config)
+
+    status_dir = make_temp_dir(os.getcwd())
+    gcs_credentials = get_gcs_credentials(_globals)
+    gcs_client = storage.Client(project=_globals['PROJECT_ID'], credentials=gcs_credentials)
+
+    local_status_file = os.path.join(status_dir, '{}.json'.format(status))
+    with open(local_status_file, 'w') as f:
+        json.dumps('0', f)
+
+    gcs_destination_bucket = gcs_client.get_bucket(bucket_or_name=_globals["MODEL_BUCKET_NAME"])
+    gcs_destination_blob = '/'.join([get_user(kwargs), get_problem(kwargs), get_version(kwargs), "STATUS",
+                                     dag_type, local_status_file.split("/")[-1]])
+    b = storage.blob.Blob(gcs_destination_blob, gcs_destination_bucket)
+    b.upload_from_filename(local_status_file, client=gcs_client)
+
+    rmtree(status_dir)
